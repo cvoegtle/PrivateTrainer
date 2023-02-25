dece961eedde7b4f664513e4324eefbcdde3accf
christian@voegtle.org <christian@voegtle.org>
2023-02-25 10:11:24 +0100

    Bluetooth connect Rechte anfordern

--- app/src/main/java/org/voegtle/privatetrainer/business/bluetooth/BluetoothPermissions.kt
+++ app/src/main/java/org/voegtle/privatetrainer/business/bluetooth/BluetoothPermissions.kt
@@ -6,6 +6,7 @@ class BluetoothPermissions {
     val code = 1
     val permissions = arrayOf(
         Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.BLUETOOTH_CONNECT,
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.BLUETOOTH_ADMIN,
